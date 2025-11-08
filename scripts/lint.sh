36c5cbec72294314e61a0f802fa85d285286b5a6
xixoan76 <danielpham18_18@icloud.com>
2025-11-09 03:54:28 +1100

    Delete scripts directory

--- scripts/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy fastapi
-ruff check fastapi tests docs_src scripts
-ruff format fastapi tests --check
--- scripts/test-cov-html.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-bash scripts/test.sh ${@}
-bash scripts/coverage.sh
